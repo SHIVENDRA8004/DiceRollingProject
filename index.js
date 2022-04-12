8912d7c745733eb5b70097f2d0daee65245bfad2
SHIVENDRA JAT <73995062+SHIVENDRA8004@users.noreply.github.com>
2022-04-12 23:31:27 +0530

    Updating the images path

--- index.js
+++ index.js
@@ -10,74 +10,69 @@ function onClick() {
     case 0:
       document
         .querySelector(".img1")
-        .setAttribute("src", "/DiceProject/images/dice1.png");
+        .setAttribute("src", "dice1.png");
       break;
 
     case 1:
       document
         .querySelector(".img1")
-        .setAttribute("src", "/DiceProject/images/dice2.png");
+        .setAttribute("src", "dice2.png");
       break;
 
     case 2:
       document
         .querySelector(".img1")
-        .setAttribute("src", "/DiceProject/images/dice3.png");
+        .setAttribute("src", "dice3.png");
       break;
 
     case 3:
       document
         .querySelector(".img1")
-        .setAttribute("src", "/DiceProject/images/dice4.png");
+        .setAttribute("src", "dice4.png");
       break;
 
     case 4:
       document
         .querySelector(".img1")
-        .setAttribute("src", "/DiceProject/images/dice5.png");
+        .setAttribute("src", "dice5.png");
       break;
 
     case 5:
       document
         .querySelector(".img1")
-        .setAttribute("src", "/DiceProject/images/dice6.png");
+        .setAttribute("src", "dice6.png");
       break;
   }
   switch (randomNumber2) {
     case 0:
       document
         .querySelector(".img2")
-        .setAttribute("src", "/DiceProject/images/dice1.png");
+        .setAttribute("src", "dice1.png");
       break;
 
     case 1:
-      document
-        .querySelector(".img2")
-        .setAttribute("src", "/DiceProject/images/dice2.png");
+      document.querySelector(".img2").setAttribute("src", "dice2.png");
       break;
 
     case 2:
       document
-        .querySelector(".img2")
-        .setAttribute("src", "/DiceProject/images/dice3.png");
+        .querySelector(".img2").setAttribute("src", "dice3.png");
       break;
 
     case 3:
       document
-        .querySelector(".img2")
-        .setAttribute("src", "/DiceProject/images/dice4.png");
+        .querySelector(".img2").setAttribute("src", "dice4.png");
       break;
 
     case 4:
       document
-        .querySelector(".img2")
-        .setAttribute("src", "/DiceProject/images/dice5.png");
+        .querySelector(".img2").setAttribute("src", "dice5.png");
       break;
 
     case 5:
       document
         .querySelector(".img2")
-        .setAttribute("src", "/DiceProject/images/dice6.png");
+        .setAttribute("src", "dice6.png");
       break;
   }
   if (randomNumber1 > randomNumber2) {
