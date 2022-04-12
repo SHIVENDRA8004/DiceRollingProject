f7c9abe7a4540061f8c371546b56166fb1c79516
SHIVENDRA JAT <73995062+SHIVENDRA8004@users.noreply.github.com>
2022-04-12 10:48:58 +0530

    Updating the paths in index.js file

--- index.js
+++ index.js
@@ -6,52 +6,52 @@ var randomNumber1=Math.floor(a);
 var randomNumber2=Math.floor(b);
 switch(randomNumber1){
     case 0:
-        document.querySelector(".img1").setAttribute("src","/DiceProject/images/dice1.png");
+        document.querySelector(".img1").setAttribute("src","/dice1.png");
         break;
 
     case 1:
-        document.querySelector(".img1").setAttribute("src","/DiceProject/images/dice2.png");
+        document.querySelector(".img1").setAttribute("src","/dice2.png");
         break;
 
     case 2:
-        document.querySelector(".img1").setAttribute("src","/DiceProject/images/dice3.png");
+        document.querySelector(".img1").setAttribute("src","/dice3.png");
         break;
     
     case 3:
-        document.querySelector(".img1").setAttribute("src","/DiceProject/images/dice4.png");
+        document.querySelector(".img1").setAttribute("src","/dice4.png");
         break;
         
     case 4:
-        document.querySelector(".img1").setAttribute("src","/DiceProject/images/dice5.png");
+        document.querySelector(".img1").setAttribute("src","/dice5.png");
         break;
     
     case 5:
-        document.querySelector(".img1").setAttribute("src","/DiceProject/images/dice6.png");
+        document.querySelector(".img1").setAttribute("src","/dice6.png");
         break;
 }
 switch(randomNumber2){
     case 0:
-        document.querySelector(".img2").setAttribute("src","/DiceProject/images/dice1.png");
+        document.querySelector(".img2").setAttribute("src","/dice1.png");
         break;
 
     case 1:
-        document.querySelector(".img2").setAttribute("src","/DiceProject/images/dice2.png");
+        document.querySelector(".img2").setAttribute("src","/dice2.png");
         break;
 
     case 2:
-        document.querySelector(".img2").setAttribute("src","/DiceProject/images/dice3.png");
+        document.querySelector(".img2").setAttribute("src","/dice3.png");
         break;
     
     case 3:
-        document.querySelector(".img2").setAttribute("src","/DiceProject/images/dice4.png");
+        document.querySelector(".img2").setAttribute("src","/dice4.png");
         break;
         
     case 4:
-        document.querySelector(".img2").setAttribute("src","/DiceProject/images/dice5.png");
+        document.querySelector(".img2").setAttribute("src","/dice5.png");
         break;
     
     case 5:
-        document.querySelector(".img2").setAttribute("src","/DiceProject/images/dice6.png");
+        document.querySelector(".img2").setAttribute("src","/dice6.png");
         break;
 }
 if(randomNumber1 > randomNumber2){
